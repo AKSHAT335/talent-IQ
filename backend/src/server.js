52227311cd89eb00c4d279445cd1274892a5f65f
Akshat Pratap Singh <ersinghakshat01@gmail.com>
2025-12-07 12:17:37 +0530

    Fix express wildcard route for Vite

--- backend/src/server.js
+++ backend/src/server.js
@@ -14,14 +14,16 @@ app.get('/books', (req, res) => {
   res.status(200).json({ message: 'this is books endpoint' });
 });
 
+if (ENV.NODE_ENV === "production") {
+  const frontendPath = path.join(__dirname, "../frontend/dist");
 
-if(ENV.NODE_ENV === 'production'){
-  app.use(express.static(path.join(__dirname, '../frontend/dist')));
+  app.use(express.static(frontendPath));
 
-  app.get('/{*any}', (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(frontendPath, "index.html"));
   });
 }
+
 
 app.listen(ENV.PORT, () => {
   console.log(`Server is running on port ${ENV.PORT}`);
